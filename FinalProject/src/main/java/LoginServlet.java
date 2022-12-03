74c35e3ccc313933ea7afdb1c604950909c6780a
roland <bllawson@unomaha.edu>
2022-12-03 00:32:44 -0600

    Add files via upload

---
+++ FinalProject/src/main/java/LoginServlet.java
@@ -0,0 +1,191 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+//import java.security.SecureRandom;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	//String dns = "ec2-44-210-87-48.compute-1.amazonaws.com";
+	String dns = "ec2-18-206-231-52.compute-1.amazonaws.com";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+        String sql;
+        Connection connection = null;
+        Statement statement = null;
+        PreparedStatement statement1 = null;
+        ResultSet rs = null;
+        PreparedStatement preparedStatement = null;
+        String username = request.getParameter("txtName");
+        String pwd = request.getParameter("password");
+        
+        //response.setContentType("text/html");            
+        //PrintWriter out = response.getWriter();
+        
+        /**
+        String title = "Display Employee Credentials";
+        String docType =
+            "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+
+        out.println(docType + //
+            "<html>\n" + //
+            "<head><title>" + title + "</title></head>\n" + //
+            "<body bgcolor = \"##CCCCFF\">\n" + //
+            "<h1 align = \"center\">" + title + "</h1>\n");
+		 */
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://" + dns + ":3306/myDB", "root", "root");
+        } catch (SQLException e2) {
+            // TODO Auto-generated catch block
+            System.out.println("Connection Failed!:\n" + e2.getMessage());
+        }
+        System.out.println("SUCCESS!!!! You made it, take control     your database now!");
+        System.out.println("Creating statement...");
+
+        sql = "SELECT * FROM Employees WHERE Username=?";
+        
+        try {
+
+            statement1 = connection.prepareStatement(sql);
+            String theUserName = username;                    
+            statement1.setString(1, theUserName);
+     
+        } catch (SQLException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
+        }
+
+        try {
+
+            rs = statement1.executeQuery();
+        } catch (SQLException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        //out.println("<table border=1 width=50% height=30%>");
+        //out.println("<tr><th>Id</th><th>Username</th><th>Salt</th><th>PasswordSalted</th>");
+        try {
+            while (rs.next()) {
+                //Retrieve by column name
+                String id = rs.getString("id");
+                String name = rs.getString("Username");
+                String salt = rs.getString("Salt");
+                String pwdsalted = rs.getString("PasswordSalted");
+                
+               // out.println("<tr><td>" + id + "</td><td>" + name + "</td><td>" + Salt + "</td><td>" + PasswordSalted + "</td></tr>");
+                
+                String securePassword = get_SHA_512_SecurePassword(pwd, salt);
+                
+                System.out.println(salt);
+                System.out.println(securePassword);
+                System.out.println(pwdsalted);
+                System.out.println(pwd);
+                
+                if (pwdsalted.equals(securePassword))
+                {
+                	
+                	response.sendRedirect("welcome.jsp");
+                	System.out.println("login successful");
+                }
+                else
+                {
+                	//out.println("<font color=red size=18> Login Failed! <br>");
+                	//out.println("<a href=login.jsp>Try Again </a>");
+                	
+                	response.sendRedirect("tryagain.jsp");
+                	System.out.println("login failed");
+                }
+            }
+            
+           // out.println("</body></html>");
+            
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+       
+        
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	// Generate Password
+	private static String get_SHA_512_SecurePassword(String passwordToHash,
+            String salt) {
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes());
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)
+                        .substring(1));
+            }
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+   
+		/*********
+	// Add Random salt
+    private static String getSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt.toString();
+    }
+    ******/
+
+}
