3096a128fe0b24a1192567c55fb9c053b2e8a98a
Travis <tjmunyer@gmail.com>
2022-12-11 14:24:00 -0600

    added more unit tests

--- finalProject/src/main/java/Tests.java
+++ finalProject/src/main/java/Tests.java
@@ -1,3 +1,6 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 import javax.mail.MessagingException;
@@ -23,8 +26,23 @@ public class Tests {
 	}
 	
 	@Test
-	public void HashTest() {
+	public void hashTest() {
 		String hash = Cryptography.get_SHA_512_SecurePassword("password", "salt");
 		Assert.assertEquals("2908d2c28dfc047741fc590a026ffade237ab2ba7e1266f010fe49bde548b5987a534a86655a0d17f336588e540cd66f67234b152bbb645b4bb85758a1325d64", hash);
+	}
+	
+	@Test
+	public void connectionTest() {
+		try {
+			Connection connection = DriverManager.getConnection(Credentials.GetDbString(), "root", Credentials.GetDBPassword());
+			Assert.assertNotNull(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void credentialsTest() {
+		Assert.assertEquals(Credentials.GetEmail(), "unocsci4830group@gmail.com");
 	}
 }
